ac88d40f2922f5c68fa7752ebd1f172051482869
meganmdawson <71418980+meganmdawson@users.noreply.github.com>
2023-04-04 20:04:09 -0400

    Updated simulation script for 2 rings

--- test_script.py
+++ test_script.py
@@ -6,11 +6,16 @@ import h5py
 import numpy as np
 import os
 
-def runSim(T = 50, gamma = 1/50, len = 10, dt = 0.1, skipSteps = 1, f_pdb = "pdb", 
-           f_ff = "forcefield"):
+def runSim(T = 50, gamma = 1/50, length = 10, dt = 0.1, skipSteps = 1, f_pdb = "pdb", 
+           f_ff = "forcefield", srSize = 20):
     """
     Function to run the openMM simulation (NVT) with given params and 
-    pdb+forcefields 
+    pdb+forcefields
+
+    Saves a xyz file of a compressed 3d array. See the function reload() to get 
+    an idea of how to load and manipulate the output array (and format the file 
+    name). The final array should have dims defined by 
+    [n_timesteps, 3 (xyz), n_particles]
 
     parameters:
     T: int
@@ -27,6 +32,8 @@ def runSim(T = 50, gamma = 1/50, len = 10, dt = 0.1, skipSteps = 1, f_pdb = "pdb
     f_ff: str
         Name of the forcefield file (excluding the file extension, which should 
         simply be .xml)
+    srSize: int
+        Number of monomer units in the small ring
     """
     # set up strings
     # THE FOLLOWING FILE NAMES ARE PLACEHOLDERS: CHANGE WHEN POSSIBLE
@@ -34,29 +41,44 @@ def runSim(T = 50, gamma = 1/50, len = 10, dt = 0.1, skipSteps = 1, f_pdb = "pdb
     pdbPath = os.path.join(path, "pdb_files/{}.pdb".format(f_pdb))
     forcefieldPath = os.path.join(path, "pdb_files/{}.xml".format(f_ff))
 
-    beta = 1./T
-    steps = int(len/dt)
-    indSteps = int(len/(dt*skipSteps))
+    # set up params
+    kB = 1.38e-23
+    beta = 1./(kB * T) # needs kb
+    steps = int(length*1000/dt)
+    indSteps = int(length*1000/(dt*skipSteps))
 
+    # load external files for topo and PES
     pdb = app.PDBFile(pdbPath)
-
     topo = pdb.topology
 
+    # identify atoms in topology
     particles = []
+    # future update maybe: collect residues to identify number of atoms/chain
+    # residues = []
     for atom in topo.atoms():
         particles.append(atom)
+        # residues.append(atom.residue)
 
-    # rough shitty code below NOT VERY ROBUST
-    carbons = particles[0:20]
-    hy1 = particles[20:40]
-    hy2 = particles[40:60]
+    # add bonds between the individual rings
+    c1 = particles[0:srSize]
+    c2 = particles[srSize:]
+    numParticles = len(particles)
+    # hy1 = particles[20:40]
+    # hy2 = particles[40:60]
 
-    for i, C in enumerate(carbons):
-        # print("Carbon: {}, {}".format(C, Cs[i-1]))
-        # print("Hydrogen: {}, {}".format(H1s[i], H2s[i]))
-        topo.addBond(C, carbons[i-1])
-        topo.addBond(C, hy1[i])
-        topo.addBond(C, hy2[i])
+    # create arrays to store xyz coords in
+    # format is timestep:xyz:particle#
+    traj = np.zeros((indSteps, 3, numParticles))
+
+    for i, C in enumerate(c1):
+        topo.addBond(C, c1[i-1])
+        # topo.addBond(C, hy1[i])
+        # topo.addBond(C, hy2[i])
+    
+    for i, C in enumerate(c2):
+        topo.addBond(C, c2[i-1])
+        # topo.addBond(C, hy1[i])
+        # topo.addBond(C, hy2[i])
 
     forcefield = app.ForceField(forcefieldPath)
     unmatched_residues = forcefield.getUnmatchedResidues(topo)
@@ -67,7 +89,8 @@ def runSim(T = 50, gamma = 1/50, len = 10, dt = 0.1, skipSteps = 1, f_pdb = "pdb
                                     nonbondedCutoff = 1e3*unit.nanometer,
                                     constraints = None)
 
-    integrator = mm.LangevinIntegrator(T*unit.kelvin, gamma, dt*unit.femtoseconds)
+    integrator = mm.LangevinIntegrator(T*unit.kelvin, gamma, 
+                                       dt*unit.femtoseconds)
 
     platform = mm.Platform.getPlatformByName('Reference')
     simulation = app.Simulation(pdb.topology, system, integrator, platform)
@@ -76,33 +99,36 @@ def runSim(T = 50, gamma = 1/50, len = 10, dt = 0.1, skipSteps = 1, f_pdb = "pdb
     	
     simulation.context.setVelocitiesToTemperature(T*unit.kelvin)
 
-    state = simulation.context.getState(getForces = True, getEnergy = True,
-                                        getPositions = True)
-    
-    # generate h5 to store info about positions of nodes in sim
-    # outH5 = h5py.File("cattraj_{T}K.h5".format(T=T))
-    # h5coords=outH5.create_dataset("coordinates",(int(steps/skipSteps)+1,
-    #                                 nAtoms,3), dtype = "f")
-    # h5kEnergy=outH5.create_dataset("kineticEnergy",(int(steps/skipSteps)+1), 
-    #                                 dtype = "f")
-    # h5pEnergy=outH5.create_dataset("potentialEnergy",(int(steps/skipSteps)+1), 
-    #                                 dtype = "f")
-    # h5coords.attrs['units'] = "nanometers"
-    # h5coords.attrs['frame'] = "lab-fixed"
-    # h5coords.attrs['temperature'] = T
-    # h5coords.attrs['steps'] = steps
-    # h5coords.attrs['skipSteps'] = skipSteps
-    # h5coords.attrs['dt'] = dt/unit.femtosecond
-    # h5coords.attrs['gamma'] = gamma
+    # equilibration step
+    # simulation.step(1000)
 
     for step in range(steps):
         simulation.step(skipSteps)
-        state = simulation.context.getState(getEnergy=True)
-        KE = state.getKineticEnergy()/unit.kilojoules_per_mole
-        PE = state.getPotentialEnergy()/unit.kilojoules_per_mole
-        print(KE)
-        print(PE)
+        # extract positions
+        current = simulation.context.getState(getPositions = True)
+        positions = np.array(current.getPositions()/unit.nanometer)
+        traj[step, :, :] = positions.transpose()
+
+    # save the trajectory as an xyz file
+    traj_r = traj.reshape(traj.shape[0], -1)
+    print(traj_r.shape)
+    np.savetxt("{}fs_{}ps_{}K_{}ss.xyz".format(dt, length, T, skipSteps), traj_r)
+
+def reload(T = 50, gamma = 1/50, length = 10, dt = 0.1, skipSteps = 1, 
+           f_pdb = "pdb", f_ff = "forcefield", srSize = 20):
+    fname = "{}fs_{}ps_{}K_{}ss.xyz".format(dt, length, T, skipSteps)
+    totSteps = int(length*1000/(skipSteps * dt))
+    print("totSteps", totSteps)
+
+    a = np.loadtxt(fname)
+    nParticles = a.shape[-1]//3
+    a = a.reshape(totSteps, 3, nParticles)
+    print(a.shape)
+
+    return a
 
 if __name__ == "__main__":
-     runSim(T = 50, gamma = 1/50, len = 1, dt = 0.1, skipSteps = 1, f_pdb = "small_ring_full", 
-           f_ff = "forcefield_sm")
+    runSim(T = 50, gamma = 1/50, length = 0.01, dt = 0.1, skipSteps = 1, 
+           f_pdb = "combo_ring", f_ff = "combo_ff")
+    # reload(T = 50, gamma = 1/50, length = 0.01, dt = 0.1, skipSteps = 1, 
+    #        f_pdb = "combo_ring", f_ff = "combo_ff")
